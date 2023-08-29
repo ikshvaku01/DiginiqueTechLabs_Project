398aaea47e2c0ef6f2828478c44221f6dd2e8cff
ikshvaku01 <yashsingh_cse@pm.me>
2023-08-29 16:41:18 +0530

    creation & modification 29.08.2023

---
+++ client/src/components/DataGridCustomColumnMenu.jsx
@@ -0,0 +1,21 @@
+import {
+    GridColumnMenuContainer,
+    GridColumnMenuFilterItem, //updated
+    GridColumnMenuHideItem, //updated
+  } from "@mui/x-data-grid";
+  
+  const CustomColumnMenu = (props) => {
+    const { hideMenu, currentColumn, open } = props;
+    return (
+      <GridColumnMenuContainer
+        hideMenu={hideMenu}
+        currentColumn={currentColumn}
+        open={open}
+      >
+        <GridColumnMenuFilterItem onClick={hideMenu} column={currentColumn} />
+        <GridColumnMenuHideItem onClick={hideMenu} column={currentColumn} />
+      </GridColumnMenuContainer>
+    );
+  };
+  
+  export default CustomColumnMenu;
